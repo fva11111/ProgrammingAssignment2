dc0ea5e801793297bfb0064d2ea485283d5b9bd7
Tatiana Vaskovskaya <tatiana.vaskovskaya@gmail.com>
2015-02-15 20:22:59 +0300

    Made two functions to calculate an inverse matrix or get cashed one if it was calculated eariler

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The script includes two functions for inversing a matrix. 
+## The matrix should be invertible. To obviate a costly computation when 
+## the inverse matrix was computed earlier the script uses cashed matrix
 
-## Write a short comment describing this function
+## Creates a  matrix  object that can cache its inverse.
+## It make a list of functions to set or get the invercible matrix 
+## and to set (setinverse) and get (getinverse) inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invx <- NULL
+  
+  ## set the matrix x, erase cashed inverse matrix
+  set <- function(y) {
+    x <<- y
+    invx <<- NULL                   
+  }
+  ## get a matrix x from the object
+  get <- function() x
+  
+  ## set inverse matrix after computataion
+  setinverse <- function(inverse) invx <<- inverse
+  
+  ## get inverse matrix exists
+  getinverse <- function() invx
+  
+  ## make an output list
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  ## get a cashed inverse matrix
+  invx <- x$getinverse()
+  ## if there is no cashed inverse matrix calculate it
+  if(is.null(invx)) {
+    data <- x$get()
+    invx <- solve(data)
+  }
+  ## write inverse matrix into list x
+  x$setinverse(invx)
+  
+  return(invx)
 }
+
